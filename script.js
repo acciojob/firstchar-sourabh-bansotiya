3cf3fdf56d0e2055bd397cd5a6115add245601dd
Sourabh Bansotiya <77042105+sourabh-bansotiya@users.noreply.github.com>
2024-04-02 04:14:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function firstChar(text) {
   // your code here
+	let empty = ('');
+	for (let i = 0; i < text.length; i++) {
+		if (text[i] == empty) {
+			return empty;
+		}
+		else{
+			return text[i][0];
+		}
+	}
 }
 
 // Do not change the code below
