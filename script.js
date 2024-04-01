e73ba5df2d009ea0c0e635044ba3dec633b996d7
Sourabh Bansotiya <77042105+sourabh-bansotiya@users.noreply.github.com>
2024-04-02 04:22:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 function firstChar(text) {
   // your code here
-	//let empty = ('');
-	//for (let i = 0; i < text.length; i++) {
-		//if (text[i] == empty) {
-			//return empty;
-		//}
-		//else{
-			//return text[i][0];
-		//}
-	//}
+	
 	for (let i = 0; i < text.length; i++) {
-        if (text[i] !== ' ') {
-            return text[i];
-        }
-    }
-    return '';
+	if (text[i] == ' ') {
+	return ' ';
+	}
+	else{
+	return text[i];
+	}
+	}
+	//for (let i = 0; i < text.length; i++) {
+      //  if (text[i] !== ' ') {
+        //    return text[i];
+        //}
+    //}
+    //return '';
 
 }
 
 // Do not change the code below
 
-const text = prompt("Enter text:");
+//const text = prompt("Enter text:");
 alert(firstChar(text));
